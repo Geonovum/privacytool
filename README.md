e3faf87f8e3df9f44ac24c25ae36d73de6c0340a
Thijs Brentjens <thijs@brentjensgeoict.nl>
2015-10-06 14:20:58 +0200

    initial commit, setup for forms, bit of styling

---
+++ README.md
@@ -0,0 +1,2 @@
+#Privacy tool webpage
+For Geonovum, create an online tool (as webpage) that helps determining what privacy policy is applicable.
---
+++ src/www/js/privacytool.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+        $("#toolform").steps({
+            headerTag: "h3",
+            bodyTag: "section",
+            // transitionEffect: "slideLeft",
+            stepsOrientation: "vertical"
+        });
+    }
+)
