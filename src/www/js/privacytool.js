52f8a9470d7185546cd0d3089f6f8c3e1faca4f8
Thijs Brentjens <thijs@brentjensgeoict.nl>
2015-11-04 14:15:25 +0100

    software en tekst licenties toegevoegd. styling en herstructurering pagina / sub pagina's

--- src/www/js/privacytool.js
+++ src/www/js/privacytool.js
@@ -1,20 +1,45 @@
 /*
 
-TODO: licensing of code
+Copyright (c) 2015 Thijs Brentjens, thijs@brentjensgeoict.nl, for Geonovum, the Netherlands.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Thijs Brentjens nor the names of
+      its contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 
 */
 /*
     config & logic
 */
 var conditionalGoto = {};
-conditionalGoto["voorvragen-A"] = {"ja": "voorvragen-B"};
-conditionalGoto["voorvragen-B"] = {"ja": "voorvragen-C"};
-conditionalGoto["voorvragen-C"] = {"ja": "voorvragen-D"};
-conditionalGoto["voorvragen-D"] = {"nee": "voorvragen-E"};
-conditionalGoto["voorvragen-E"] = {"nee": "voorvragen-F"};
-conditionalGoto["voorvragen-F"] = {"nee": "voorvragen-G"};
-conditionalGoto["voorvragen-G"] = {"nee": "voorvragen-H"};
-conditionalGoto["voorvragen-H"] = {"ja": "intake"};
+conditionalGoto["voorvragen-A"] = {"ja":  "#voorvragen-B"};
+conditionalGoto["voorvragen-B"] = {"ja":  "#voorvragen-C"};
+conditionalGoto["voorvragen-C"] = {"ja":  "#voorvragen-D"};
+conditionalGoto["voorvragen-D"] = {"nee": "#voorvragen-E"};
+conditionalGoto["voorvragen-E"] = {"nee": "#voorvragen-F"};
+conditionalGoto["voorvragen-F"] = {"nee": "#voorvragen-G"};
+conditionalGoto["voorvragen-G"] = {"nee": "#voorvragen-H"};
+conditionalGoto["voorvragen-H"] = {"ja":  ".actions"};
 
 /*
     Functions
@@ -136,6 +161,13 @@ $(document).ready(function() {
 )
 
 
+function startVoorvragen(){
+    $('#voorvragen').show();
+    // $('.actions').show();
+    return false;
+}
+
+// _prevElement = "";
 function checkRadioQuestion(answerId, radioName, radioValue) {
     $(".explanation."+radioName).hide();
     $(".explanation."+radioName + "." + radioValue).show();
@@ -144,11 +176,16 @@ function checkRadioQuestion(answerId, radioName, radioValue) {
     // hide explanation for non-answers
     // show explanation
     // showExplanation(answerId)
+    // var topMargin = 0;
     try {
         if (conditionalGoto[radioName][radioValue]) {
-            // console.log(conditionalGoto[radioName][radioValue])            
-            $("#"+conditionalGoto[radioName][radioValue]).show();
-            scrollToElement(conditionalGoto[radioName][radioValue]);
+            // console.log(conditionalGoto[radioName][radioValue])
+
+            $(conditionalGoto[radioName][radioValue]).show();
+            // scroll to the previous element?
+            /* if (_prevElement.length > 0) scrollToElement(_prevElement, topMargin);
+            _prevElement = conditionalGoto[radioName][radioValue];
+            */
         }
     } catch (e) {
         if (console) console.log(e)
@@ -183,8 +220,9 @@ function toggleIntake() {
     $("#intakecopy").toggle();
 }
 
-function scrollToElement(elementId) {
-    $(window).scrollTop($("#" + elementId).offset().top)
+function scrollToElement(elementId, topMargin) {
+    if (!topMargin) topMargin = 0;
+    $(window).scrollTop($("#" + elementId).offset().top - topMargin)
 }
 
 function calculateScores() {
